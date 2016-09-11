ab004a24a86b1ec6efab8ac1a794b85fb7f33ccc
Jayson Reis (jaysonsantos) <santosdosreis@gmail.com>
2016-09-11 16:55:35 +0200

    Parse create/update feeds on tasks

--- deinfoxication/feed/tasks.py
+++ deinfoxication/feed/tasks.py
@@ -1,9 +1,12 @@
 """Feed's tasks."""
+from datetime import datetime
+
 import feedparser
+import pytz
 from lxml.html import document_fromstring
 from sqlalchemy.orm.exc import NoResultFound
 
-from deinfoxication import celery
+from deinfoxication import celery, db
 from deinfoxication.feed.models import Article, Feed
 from deinfoxication.utils import app_context
 
@@ -15,6 +18,7 @@ def update_feeds():
         update_feed.apply_async((feed.id,))
 
 
+@celery.task
 @app_context
 def update_feed(feed_id: str) -> None:
     """Fetch a feed and apply the difference on the database."""
@@ -23,6 +27,8 @@ def update_feed(feed_id: str) -> None:
         raise update_feed.retry(max_retries=1, countdown=60)  # Maybe not commited
     # Let's start the caching with etag first and then if needed we can check if other types are available
     parsed_feed = feedparser.parse(feed.url, feed.last_etag)
+    if parsed_feed.status == 304:
+        return
     feed.name = parsed_feed.feed.title  # Is it right to always override?
     feed.last_etag = parsed_feed.etag
 
@@ -33,9 +39,16 @@ def update_feed(feed_id: str) -> None:
             article = Article()
         article.feed = feed
         article.title = entry.title
-        doc = document_fromstring(
-            entry.content[0].value if hasattr(entry, 'content') else entry.summary)
+        article.url = entry.link
+        last_date = entry.updated_parsed if 'updated_parsed' in entry else entry.published_parsed
+        article.publication_date = datetime(*list(last_date)[:6], tzinfo=pytz.UTC)
+        raw_text = entry.content[0].value if hasattr(entry, 'content') else entry.summary
+        article.html_text = raw_text
+        doc = document_fromstring(raw_text)
         article.clean_text = doc.text_content()  # TODO: Clear an try to match up by content type
+        db.session.add(article)
+
+    db.session.commit()
 
 
 @celery.task
--- tests/feed/test_tasks.py
+++ tests/feed/test_tasks.py
@@ -1,12 +1,16 @@
 """Test feed tasks."""
+from tests.factories import FeedFactory
+
 from deinfoxication import db
 from deinfoxication.feed.tasks import update_feed
-from tests.factories import FeedFactory
 
 
 def test_update_feed(app):
     """Test feed update."""
     feed = FeedFactory()
     db.session.commit()
-    update_feed(feed.id)
-    assert len(feed.articles) == 1022
+    update_feed.apply_async((feed.id,))
+    assert len(feed.articles) == 40
+
+    update_feed.apply_async((feed.id,))
+    assert len(feed.articles) == 40
